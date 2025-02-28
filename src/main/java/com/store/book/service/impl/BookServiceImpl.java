d0e28eb7812180136df6ea86d86b94973e8afb91
Volodymyr Mykychak <leadervlo.d@gmail.com>
2025-02-28 20:23:30 +0200

    🧹 Removed redundant empty lines and spaces in AuthorSpecificationProvider.java

--- src/main/java/com/store/book/service/impl/BookServiceImpl.java
+++ src/main/java/com/store/book/service/impl/BookServiceImpl.java
@@ -17,23 +17,18 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
-
     private final BookRepository bookRepository;
-
     private final BookMapper bookMapper;
-
     private final BookSpecificationBuilder bookSpecificationBuilder;
 
     @Override
     public BookDto save(CreateBookRequestDto bookDto) {
-
         Book book = bookMapper.toModel(bookDto);
         return bookMapper.toDto(bookRepository.save(book));
     }
 
     @Override
     public List<BookDto> findAll() {
-
         return bookRepository.findAll()
                 .stream()
                 .map(bookMapper::toDto)
@@ -42,7 +37,6 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BookDto findById(Long id) {
-
         Book book = bookRepository.findById(id).orElseThrow(() ->
                 new EntityNotFoundException("Can't find book by id " + id));
         return bookMapper.toDto(book);
@@ -50,13 +44,11 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public void deleteById(Long id) {
-
         bookRepository.deleteById(id);
     }
 
     @Override
     public List<BookDto> search(BookSearchParametersDto searchParametersDto) {
-
         Specification<Book> build = bookSpecificationBuilder.build(searchParametersDto);
         return bookRepository.findAll(build)
                 .stream()
