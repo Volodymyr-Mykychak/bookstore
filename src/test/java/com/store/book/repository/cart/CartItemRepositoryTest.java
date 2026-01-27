b8da154ce76af67b95c60a5496fb6e0c597edd88
Volodymyr Mykychak <leadervlo.d@gmail.com>
2026-01-27 13:18:48 +0200

    fix: resolve primary key violation by cleaning books table between tests

--- src/test/java/com/store/book/repository/cart/CartItemRepositoryTest.java
+++ src/test/java/com/store/book/repository/cart/CartItemRepositoryTest.java
@@ -9,10 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class CartItemRepositoryTest {
 
     @Autowired
