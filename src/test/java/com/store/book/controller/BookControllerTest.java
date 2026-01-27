6c210a7aa6171685cab0221ad7cf90ce8b80e2bf
Volodymyr Mykychak <leadervlo.d@gmail.com>
2026-01-27 10:46:37 +0200

    fix import

--- src/test/java/com/store/book/controller/BookControllerTest.java
+++ src/test/java/com/store/book/controller/BookControllerTest.java
@@ -1,9 +1,7 @@
 package com.store.book.controller;
 
-import com.store.book.dto.book.BookDto;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import org.springframework.test.web.servlet.MvcResult;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -12,6 +10,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.store.book.dto.book.BookDto;
 import com.store.book.dto.book.CreateBookRequestDto;
 import com.store.book.util.TestDataHelper;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,6 +23,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
